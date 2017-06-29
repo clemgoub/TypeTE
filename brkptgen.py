14545d1b0c307c6716f7633eaa065aa6720d6e80
Jeff Kidd <jmkidd@med.umich.edu>
2017-06-29 10:33:31 -0400

    setup of making alternative alleles

---
+++ brkptgen.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+
+###############################################################################
+# Helper function to run commands, handle return values and print to log file
+def runCMD(cmd):
+    val = subprocess.Popen(cmd, shell=True).wait()
+    if val == 0:
+        pass
+    else:
+        print 'command failed'
+        print cmd
+        sys.exit(1)
+###############################################################################
+###############################################################################
+def add_breaks_to_line(seq,n=50):
+    myList = []
+    myList = [i for i in seq]
+    newList = []
+    c = 0
+    for i in myList:
+        newList.append(i)
+        c += 1
+        if c % n == 0 and c != (len(myList)):
+            newList.append('\n')
+    myStr = ''.join(newList)
+    return myStr    
+###############################################################################
