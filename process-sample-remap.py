fb2986df0d5dbe559baa759079ecb5692efefb22
Jeff Kidd <jmkidd@med.umich.edu>
2017-08-07 11:26:42 -0400

    rerurn just mapping

--- process-sample-remap.py
+++ process-sample-remap.py
@@ -31,11 +31,12 @@ def get_site_intervals_from_table(bpIntervalTable):
 
 ###############################################################################
 USAGE = """
-python process-sample.py   --allelefile <allele file in proper format>
+python process-sample-remap.py   --allelefile <allele file in proper format>
                            --allelebase <base directory for allele information>
                            --bwa <path/cmd for bwa 0.5.9
                            --samplename <name of sample>
-                           --bam <indexed bam of sample>
+
+Version assumes reads extracted already from the BAM, only does that mapping steps
 
 
 """
@@ -44,8 +45,6 @@ parser.add_option('--allelefile',dest='alleleFile', help = 'file name of allele 
 parser.add_option('--allelebase',dest='alleleBase', help = 'base dir name for outputs')
 parser.add_option('--bwa',dest='bwa', help = 'path/cmd for bwa 0.7.15')
 parser.add_option('--samplename',dest='sampleName', help = 'sample name')
-parser.add_option('--bam',dest='bam', help = 'bam of sample')
-
 
 (options, args) = parser.parse_args()
 
@@ -57,9 +56,6 @@ if options.bwa is None:
     parser.error('bwa cmd given')
 if options.sampleName is None:
     parser.error('sample name not given')
-if options.bam is None:
-    parser.error('bam  not  given')
-
 
 
 ###############################################################################
@@ -70,7 +66,6 @@ if options.bam is None:
 myData = {}
 myData['bwa'] = options.bwa
 myData['sampleName'] = options.sampleName
-myData['bam'] = options.bam
 myData['alleleBase'] = options.alleleBase
 if myData['alleleBase'][-1] != '/' :
     myData['alleleBase'] += '/'
@@ -82,14 +77,6 @@ myData['siteIntervals'] = brkptgen.get_site_intervals_from_table(options.alleleF
 #myData['siteIntervals'] = myData['siteIntervals'][0:10]  
 
 print 'Found %i siteIntervals' % len(myData['siteIntervals'])
-
-
-print 'Will do sample name %s' % myData['sampleName']
-print 'Bam file: %s' % myData['bam']
-if os.path.isfile(myData['bam']) is False:
-    print 'ERROR! BAM FILE Does not exist'
-    print myData['bam']
-    sys.exit()
 
 
 
@@ -124,129 +111,8 @@ myData['outputSAMFile'] = myData['sampleBase'] + '%s.intervalreads.sam' %  myDat
 
 print 'Have %i intervals to process' % len(myData['siteIntervals'])
 
-# here, go through each site interval and get all reads that overlap....
-outFile = open(myData['outPutReadFile'],'w')
-outSAM = open(myData['outputSAMFile'],'w')
-rN = 0
-for call in myData['siteIntervals']:
-    samLines = {}
-    rpToDo = {}
-    rpToFind = {}
-    
-    rN += 1
-    if rN % 20 == 0:
-        print '\n*****\nDoing %i of %i ...\n*****\n' % (rN,len(myData['siteIntervals']))
-    id = call[0]
-    c = call[1][0]
-    b = call[1][1]
-    e = call[1][2]
-    reg = '%s:%i-%i' % (c,b,e)
-    print reg
-    numReads = 0
-    numSC = 0
-    bamIn = brkptgen.open_bam_read(myData['bam'],reg)
-    for line in bamIn:
-        numReads += 1
-        ol = line
-        line = line.rstrip()
-        line = line.split()
-        samParse = brkptgen.parse_sam_line(line)
-        if samParse['isFirst'] is True:
-            rNum = '1'
-        else:
-            rNum = '2'
-        samLines[(samParse['seqName'],rNum)] = ol
-
-        #get rid of things that are not good
-        if samParse['unMapped'] is True:
-            continue
-        if samParse['isDuplicate'] is True:
-            continue
-        if samParse['notPrimaryAlignment'] is True:
-            continue
-        if samParse['mapQ'] < myData['min_map_q']:
-            continue
-        if samParse['isPaired'] is False:
-            continue
-
-        numSC += 1
-        outFile.write('%s\t%s\t%i\t%i\t%s\n' % (id,samParse['seqName'],samParse['flag'],samParse['mapQ'],samParse['cigar']))
-        rpToDo[samParse['seqName']] = 1
-    bamIn.close()
-    print 'Reads considered: %i, found: %i' % (numReads,numSC)
-    print 'Found %i rp to do' % len(rpToDo)
-    print 'initial sam dict contains %i reads' % len(samLines)
-
-    #now want to go through list and print out sam lines for ones that we need
-    numWritten = 0
-    for rn in rpToDo:
-        for rNum in ['1','2']:
-            if (rn,rNum) in samLines:
-                samLine = samLines[(rn,rNum)]
-                outSAM.write(samLine)
-                numWritten += 1
-            else:
-                rpToFind[(rn,rNum)] = 1
-    print 'After first pass, we have %i reads to find' % len(rpToFind)
-    for (rn,rNum) in rpToFind:
-        # check since we had updated
-        if (rn,rNum) in samLines:
-            samLine = samLines[(rn,rNum)]
-            outSAM.write(samLine)
-            numWritten += 1
-            continue
-        
-        if rNum == '1':
-            otherNum = '2'
-        elif rNum == '2':
-            otherNum = '1'
-            
-        samLine = samLines[(rn,otherNum)]
-        samLine = samLine.rstrip()
-        samLine = samLine.split()
-        samParse = brkptgen.parse_sam_line(samLine)
-        #genutils.print_dictionary_keys(samParse)
-        if samParse['mateChrom'] == '=':
-            newReg = samParse['chrom'] + ':' + samParse['matePos'] + '-' + samParse['matePos']
-        else:
-            newReg = samParse['mateChrom'] + ':' + samParse['matePos'] + '-' + samParse['matePos']
-         
-        if samParse['mateUnmapped'] is True:
-            print 'Mate is unmapped, not sure what to do, trying mapped region'
-#            print samLine
-            newReg = samParse['chrom'] + ':' + str(samParse['chromPos']) + '-' + str(samParse['chromPos'])
-
-
-        bamIn = brkptgen.open_bam_read(myData['bam'],newReg)
-        for line in bamIn:
-            numReads += 1
-            ol = line
-            line = line.rstrip()
-            line = line.split()
-            samParse = brkptgen.parse_sam_line(line)
-            if samParse['isFirst'] is True:
-                r = '1'
-            else:
-                r = '2'
-            samLines[(samParse['seqName'],r)] = ol
-        bamIn.close()
-        
-        #now do the check
-        if (rn,rNum) in samLines:
-            samLine = samLines[(rn,rNum)]
-            outSAM.write(samLine)
-            numWritten += 1
-        else:
-            print 'cannot find!',rn,rNum
-            sys.exit()
-
-    print 'Wrote out',numWritten
-outFile.close()
-outSAM.close()
-
-
 # at this point, we have extracted all of the reads for the sample!
-print 'Have extracted all reads for the sample!'
+print 'Assuming have extracted all reads for the sample!'
 
 print 'Begin map to alt alleles steps!'
 
@@ -356,7 +222,7 @@ myData['lhSummaryFileName'] =  myData['sampleBase'] + myData['sampleName'] + '.l
 myData['outVCFName'] = myData['sampleBase'] + myData['sampleName'] + '.vcf'
 outVCF = open(myData['outVCFName'],'w')
 outVCF.write('##fileformat=VCFv4.1\n')
-outVCF.write('##fileDate=20170629\n')
+outVCF.write('##fileDate=20170807\n')
 outVCF.write('##reference=canFam3.1\n')
 outVCF.write('##FORMAT=<ID=AD,Number=.,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">\n')
 outVCF.write('##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth (only filtered reads used for calling)">\n')
@@ -364,7 +230,7 @@ outVCF.write('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n')
 outVCF.write('##FORMAT=<ID=GQ,Number=1,Type=Float,Description="Genotype Quality">\n')
 outVCF.write('##FORMAT=<ID=PL,Number=3,Type=Float,Description="Normalized, Phred-scaled likelihoods for AA,AB,BB genotypes where A=ref and B=alt">\n')
 
-outVCF.write('##ALT=<ID=INS:ME:HERVK,Description="Insertion of HERVK element">\n')
+outVCF.write('##ALT=<ID=INS:ME:CfERV,Description="Insertion of HERVK element">\n')
 outVCF.write('##INFO=<ID=DP,Number=1,Type=Integer,Description="Filtered Depth">\n')
 header = ['#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT']
 
