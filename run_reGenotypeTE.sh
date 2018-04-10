7a04cfb1a3587292d020ae8a85aab7a106352c59
Clement Goubert <goubert.clement@gmail.com>
2018-04-10 15:41:36 -0600

    add splitfile step

--- run_reGenotypeTE.sh
+++ run_reGenotypeTE.sh
@@ -30,9 +30,13 @@ mkdir -p $OUTDIR/$PROJECT
 ./input_from_melt.sh $VCF $PROJECT
 
 
-#Join ind names and bam files to generate the list of locus/individuals
+#Join ind names to coordinates and generate the list of locus/individuals ("$OUTDIR/$PROJECT/sample_file.txt.list.txt")
 
 perl makelist_v1.0.pl -t $BAMFILE -f $OUTDIR/$PROJECT/$PROJECT.input -p $OUTDIR/$PROJECT
+
+# split the input in order to paralellize read extraction
+
+perl 02_splitfile_jt_v3.0_pipeline.pl -f $OUTDIR/$PROJECT/sample_file.txt.list.txt -s yes -n $individual_nb
 
 #splitfile.pl -out $OUTDIR uses file.list.text based on -individuals --> splitbyindividuals folder created with one file per individual_nb + list_of_splitted_files_names in $OUTDIR
 
