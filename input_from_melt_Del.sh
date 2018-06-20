90dddcaa18088d7d933ba78a715ac36399b05f94
Clement Goubert <goubert.clement@gmail.com>
2018-06-20 18:36:38 -0400

    fix make input for deletions

--- input_from_melt_Del.sh
+++ input_from_melt_Del.sh
@@ -16,16 +16,14 @@ if [[ $testfile == "gzip" ]]
 then
 
 headline=$(zcat $1 | grep -n "#CHROM" | cut -d : -f 1)
-TSD=$(zcat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "TSD") printf i } } ')
 MEINFO=$(zcat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "MEINFO") printf i } } ')
-zcat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g' | awk '{print $1,$2,$3,$5,$8,$4}' > $OUTDIR/$PROJECT/$2"".input
+zcat $1 | | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g' | awk '{print $1,$2,$3,$4}' > $OUTDIR/$PROJECT/$2"".input
 
 else
 
 headline=$(cat $1 | grep -n "#CHROM" | cut -d : -f 1)
-TSD=$(cat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "TSD") printf i } } ')
 MEINFO=$(cat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "MEINFO") printf i } } ')
-cat $1 | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g' | awk '{print $1,$2,$3,$5,$8,$4}' > $OUTDIR/$PROJECT/$2"".input
+cat $1 | | awk -v varline="$headline" -F $'\t' '/SVTYPE=DEL_ALU/ && NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g' | awk '{print $1,$2,$3,$4}' > $OUTDIR/$PROJECT/$2"".input
 
 
 fi
