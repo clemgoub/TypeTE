0524ab656e6d542fbdf2639fc4d40f62094512eb
jainyt <jainythomas1@gmail.com>
2019-04-17 15:16:38 -0600

    fixed duplicates issue

--- 01_DelP_findcorrespondinginsertion_v3.1.pl
+++ 01_DelP_findcorrespondinginsertion_v3.1.pl
@@ -15,7 +15,7 @@ use Cwd;
 use Data::Dumper;
 
 
-my $version = "3.2";
+my $version = "3.0";
 my $scriptname = "findcorrespondingTEinsertions.pl";
 my $changelog = "
 #   - v1.0 = 30 July 2017 
@@ -28,8 +28,7 @@ my $changelog = "
 #	- v3.1 = 10 January 2019
 #				introduced another round of finding RM cordinates for which breakpoints were more 100 bp far, the new cut off is 220bp.
 #				 
-#	- v3.2 = 15 April 2019
-#				modified to remove duplicate entries from Repeatmasker as there are other older Alu elements nearby the breakpoint
+#	
 #				
 #
 \n";
@@ -74,8 +73,8 @@ my $cwd = getcwd();
 $path = $cwd if (!$path) ;
 $rawout = "$path/file.correspondingRepeatMaskerTEs.txt" if (!$rawout);
 my $logfile = "$path/fileRMfailed.log";
-#my $loginter = "$path/fileRMfailed.inter.log";
-#my $wholeout = "$path/file.withbrkpointsRMcordinates.txt";
+my $loginter = "$path/fileRMfailed.inter.log";
+#my $wholeout = "$file.withbrkpointsRMcordinates.txt";
 #-----------------------------------------------------------------------------
 #----------------------------------- MAIN ------------------------------------
 #-----------------------------------------------------------------------------
@@ -106,10 +105,9 @@ open (my $fh, "<", $file) or confess "\n ERROR (main): could not open to read $f
 		my $brkpont = $col[2];
 		$rip = $col[0];
 		#$found = $rip;
-		
+		push (@logvalues,$rip);
 		my $num = 50;
-		my $foundvalue = &find_element($num,$chro,$brkpont,$rip);
-		push (@logvalues,$rip) if ($foundvalue ==1);
+		&find_element($num,$chro,$brkpont,$rip);
 	}
 #&print_array($rawout,@valuestoprint);
 #&print_array($loginter,@logvalues)
@@ -132,9 +130,7 @@ if (@logvalues) {
 	print STDERR "Please check logfile for values for which Reference element not identified...\n";
 	&print_array($logfile,@final_log);
 }
-#&print_array($rawout,@valueswithbrkpnt);
-#print Dumper %brkpointcorre,"\n";
-&print_keyvalue($rawout,%brkpointcorre);
+&print_array($rawout,@valueswithbrkpnt);
 exit;
 #-----------------------------------------------------------------------------
 #----------------------------------- SUB -------------------------------------
@@ -155,7 +151,7 @@ sub load_file {
 
 sub find_element {
 	my ($number,$chrom,$brekpont,$marker) = @_;
-	#print "$number,$chrom,$brekpont,$marker are number chromo and brekpoint and marker\n";
+	#print "$number,$chrom,$brekpont are number chromo and brekpoint\n";
 	my $found = 1;
 	foreach my $chromosome (sort keys %repeatmaskerfile) {
 		my $chromo;
@@ -164,44 +160,37 @@ sub find_element {
 		my $startplus;
 		my $te;
 		my $len;
-		my $end;
 		
 		
 		if ($chrom eq $chromosome) {
 			foreach my $whline (sort keys %{ $repeatmaskerfile{$chromosome}}) {#sort the next level of keys
-				
+				#print "$whline\n";
 				$chromo = $repeatmaskerfile{$chromosome}{$whline}->[0];
 				$start = $repeatmaskerfile{$chromosome}{$whline}->[1];
 				$end = $repeatmaskerfile{$chromosome}{$whline}->[2];
 				$te = $repeatmaskerfile{$chromosome}{$whline}->[3];
-				$len = ($end-$start)+1;
+				$len = $end-$start;
 				$startminus = ($start - $number);
 				$startplus = ($start + $number);
 				if (($brekpont >= $startminus) && ($brekpont <= $startplus) && ($te =~ /Alu.*/)) {
-					#print "$whline\n";
 					print STDERR "$marker found!\n";
 					#print STDERR "$whline\n";
-					#my $withbrkp = "$marker\t$whline"; 
+					my $withbrkp = "$marker\t$whline"; 
 					#push (@valuestoprint,$whline);
 					if (exists $brkpointcorre{$marker}) {
 						my $valueline= $brkpointcorre{$marker};
-						my @colums = split(/\t/,$valueline);
-						my $telen= ($colums[2]-$colums[1])+1;
-						print "the length of the TE $te is $telen\n";
+						my $colums = split(/\t/,$valueline);
+						my $telen= ($columns[2]-$columns[1])+1;
 						if ($len > $telen) {
 							$brkpointcorre{$marker}=$whline;
-							$found = 2;
-							#$found = 2 if ($len > 60);
 						}
 					} else {
 						$brkpointcorre{$marker}=$whline;
-						$found = 2;
-						#$found = 2 if ($len >60);
 					}
 					
-					#push (@valueswithbrkpnt,$withbrkp);				
-					#pop (@logvalues);
-					
+					push (@valueswithbrkpnt,$withbrkp);				
+					pop (@logvalues);
+					$found = 2;
 				} 
 			}
 		} else {
@@ -219,14 +208,5 @@ sub print_array {
 			print $fhout "$dataline\n";
 		}
 	close ($fhout);
-}
-
-sub print_keyvalue {
-	my ($filepath,%hashtoprint) = @_;
-	open (my $kp,">","$filepath") || die ("failed to open file to write $filepath $!\n");
-	foreach my $mate (sort keys %hashtoprint) {
-		print $kp "$mate\t$hashtoprint{$mate}\n";
-	}
-	close $kp;
 }
 
