abcf6df667a22f6477f6c2a1aab3302cc14f1a1b
clemgoub <goubert.clement@gmail.com>
2019-05-22 10:34:27 -0400

    last working version Alu and genotyping

--- 01_DelP_findcorrespondinginsertion_v3.0.pl
+++ 01_DelP_findcorrespondinginsertion_v3.0.pl
@@ -15,7 +15,7 @@ use Cwd;
 use Data::Dumper;
 
 
-my $version = "3.0";
+my $version = "3.2";
 my $scriptname = "findcorrespondingTEinsertions.pl";
 my $changelog = "
 #   - v1.0 = 30 July 2017 
@@ -28,7 +28,8 @@ my $changelog = "
 #	- v3.1 = 10 January 2019
 #				introduced another round of finding RM cordinates for which breakpoints were more 100 bp far, the new cut off is 220bp.
 #				 
-#	
+#	- v3.2 = 15 April 2019
+#				modified to remove duplicate entries from Repeatmasker as there are other older Alu elements nearby the breakpoint
 #				
 #
 \n";
@@ -73,7 +74,8 @@ my $cwd = getcwd();
 $path = $cwd if (!$path) ;
 $rawout = "$path/file.correspondingRepeatMaskerTEs.txt" if (!$rawout);
 my $logfile = "$path/fileRMfailed.log";
-#my $wholeout = "$file.withbrkpointsRMcordinates.txt";
+#my $loginter = "$path/fileRMfailed.inter.log";
+#my $wholeout = "$path/file.withbrkpointsRMcordinates.txt";
 #-----------------------------------------------------------------------------
 #----------------------------------- MAIN ------------------------------------
 #-----------------------------------------------------------------------------
@@ -88,6 +90,7 @@ my @logvalues;
 my @valueswithbrkpnt;
 my $found;
 my $rip;
+my %brkpointcorre;
 
 make_path("$path") if ($path);
 print STDERR "loading the Repeatmasker data....\n";
@@ -103,12 +106,14 @@ open (my $fh, "<", $file) or confess "\n ERROR (main): could not open to read $f
 		my $brkpont = $col[2];
 		$rip = $col[0];
 		#$found = $rip;
-		push (@logvalues,$rip);
+		
 		my $num = 50;
-		&find_element($num,$chro,$brkpont,$rip);
+		my $foundvalue = &find_element($num,$chro,$brkpont,$rip);
+		push (@logvalues,$rip) if ($foundvalue ==1);
 	}
 #&print_array($rawout,@valuestoprint);
-
+#&print_array($loginter,@logvalues)
+print (@logvalues);
 if (@logvalues) {
 	my (@remvalues) = @logvalues ;
 	my @final_log;
@@ -127,7 +132,9 @@ if (@logvalues) {
 	print STDERR "Please check logfile for values for which Reference element not identified...\n";
 	&print_array($logfile,@final_log);
 }
-&print_array($rawout,@valueswithbrkpnt);
+#&print_array($rawout,@valueswithbrkpnt);
+#print Dumper %brkpointcorre,"\n";
+&print_keyvalue($rawout,%brkpointcorre);
 exit;
 #-----------------------------------------------------------------------------
 #----------------------------------- SUB -------------------------------------
@@ -140,7 +147,7 @@ sub load_file {
 			chomp $data; #storing the table values to the $data and removing the extraline
 			my @namebam = split(/\s+/,$data); # splitting the data based on tab and storing into the arrray
 			my $chr = $namebam[0];
-			$sbamfile{$chr}{$data} = [($namebam[0],$namebam[1],$namebam[3])]; #loading to the hash
+			$sbamfile{$chr}{$data} = [($namebam[0],$namebam[1],$namebam[2],$namebam[3])]; #loading to the hash
 		}
 	return (%sbamfile);
 	close $th;	
@@ -148,7 +155,7 @@ sub load_file {
 
 sub find_element {
 	my ($number,$chrom,$brekpont,$marker) = @_;
-	#print "$number,$chrom,$brekpont are number chromo and brekpoint\n";
+	#print "$number,$chrom,$brekpont,$marker are number chromo and brekpoint and marker\n";
 	my $found = 1;
 	foreach my $chromosome (sort keys %repeatmaskerfile) {
 		my $chromo;
@@ -156,24 +163,45 @@ sub find_element {
 		my $startminus;
 		my $startplus;
 		my $te;
+		my $len;
+		my $end;
 		
 		
 		if ($chrom eq $chromosome) {
 			foreach my $whline (sort keys %{ $repeatmaskerfile{$chromosome}}) {#sort the next level of keys
-				#print "$whline\n";
+				
 				$chromo = $repeatmaskerfile{$chromosome}{$whline}->[0];
 				$start = $repeatmaskerfile{$chromosome}{$whline}->[1];
-				$te = $repeatmaskerfile{$chromosome}{$whline}->[2];
+				$end = $repeatmaskerfile{$chromosome}{$whline}->[2];
+				$te = $repeatmaskerfile{$chromosome}{$whline}->[3];
+				$len = ($end-$start)+1;
 				$startminus = ($start - $number);
 				$startplus = ($start + $number);
 				if (($brekpont >= $startminus) && ($brekpont <= $startplus) && ($te =~ /Alu.*/)) {
+					#print "$whline\n";
 					print STDERR "$marker found!\n";
 					#print STDERR "$whline\n";
-					my $withbrkp = "$marker\t$whline"; 
+					#my $withbrkp = "$marker\t$whline"; 
 					#push (@valuestoprint,$whline);
-					push (@valueswithbrkpnt,$withbrkp);				
-					pop (@logvalues);
-					$found = 2;
+					if (exists $brkpointcorre{$marker}) {
+						my $valueline= $brkpointcorre{$marker};
+						my @colums = split(/\t/,$valueline);
+						my $telen= ($colums[2]-$colums[1])+1;
+						print "the length of the TE $te is $telen\n";
+						if ($len > $telen) {
+							$brkpointcorre{$marker}=$whline;
+							$found = 2;
+							#$found = 2 if ($len > 60);
+						}
+					} else {
+						$brkpointcorre{$marker}=$whline;
+						$found = 2;
+						#$found = 2 if ($len >60);
+					}
+					
+					#push (@valueswithbrkpnt,$withbrkp);				
+					#pop (@logvalues);
+					
 				} 
 			}
 		} else {
@@ -193,3 +221,11 @@ sub print_array {
 	close ($fhout);
 }
 
+sub print_keyvalue {
+	my ($filepath,%hashtoprint) = @_;
+	open (my $kp,">","$filepath") || die ("failed to open file to write $filepath $!\n");
+	foreach my $mate (sort keys %hashtoprint) {
+		print $kp "$mate\t$hashtoprint{$mate}\n";
+	}
+	close $kp;
+}
