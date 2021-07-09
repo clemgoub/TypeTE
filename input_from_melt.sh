7f1c7b822f478cd24c288d9bde428e79c037f1c7
clemgoub <goubert.clement@gmail.com>
2021-07-09 07:19:52 -0400

    remove " from input

--- input_from_melt.sh
+++ input_from_melt.sh
@@ -15,14 +15,14 @@ then
 headline=$(zcat $1 | grep -n "#CHROM" | cut -d : -f 1)
 TSD=$(zcat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "TSD") printf i } } ')
 MEINFO=$(zcat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "MEINFO") printf i } } ')
-zcat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g;s/:/\t/g' | awk '{print $1,$2,$3,$5,$NF,$4}' > $OUTDIR/$PROJECT/$2"".input
+zcat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g;s/:/\t/g;s/"//g' | awk '{print $1,$2,$3,$5,$NF,$4}' > $OUTDIR/$PROJECT/$2"".input
 
 else
 
 headline=$(cat $1 | grep -n "#CHROM" | cut -d : -f 1)
 TSD=$(cat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "TSD") printf i } } ')
 MEINFO=$(cat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | head -n 1 | sed 's/;/\t/g' |  awk '{for (i=1; i<=NF; ++i) { if ($i ~ "MEINFO") printf i } } ')
-cat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g;s/:/\t/g' | awk '{print $1,$2,$3,$5,$NF,$4}' > $OUTDIR/$PROJECT/$2"".input
+cat $1 | awk -v varline="$headline" -F $'\t' 'NR > varline {print $1,$2,$8}' | sed 's/;/\t/g' | awk -v tsd="$TSD" -v meifo="$MEINFO" '{print $1"_"$2,$1,$2, $tsd, $meifo}' | sed 's/,/\t/g;s/TSD=//g;s/MEINFO=//g;s/:/\t/g;s/"//g' | awk '{print $1,$2,$3,$5,$NF,$4}' > $OUTDIR/$PROJECT/$2"".input
 
 
 fi
